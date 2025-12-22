c1a211feeee2a160cb9db6fdfee980e5e956f3f3
Sumedh <56512410+sumedh27@users.noreply.github.com>
2025-12-22 09:37:52 +0000

    Add Loading

--- src/index.js
+++ src/index.js
@@ -8,4 +8,10 @@ import playerScreen from './ui/playerScreen';
     const player = game.getPlayers()[0];
     const computer = game.getPlayers()[1];
     playerScreen(game, player, computer, ships);
+
+    const loader = document.getElementById('loader');
+    if (loader) {
+        loader.classList.add('fade-out');
+        setTimeout(() => loader.remove(), 400);
+    }
 })();
